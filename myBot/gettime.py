cadef5458a985685d27b0089dce16f96471d18e5
pamPammaaP <fesfvvgjhh@gmail.com>
2025-03-02 16:05:57 +0300

    Рабочая черновая версия

--- myBot/gettime.py
+++ myBot/gettime.py
@@ -4,19 +4,21 @@ from random import randint
 
 def get_time():
 
-    if datetime.now().hour < 22 and 9 < datetime.now().hour:
+    """if datetime.now().hour < 22 and 9 < datetime.now().hour:
         hourstime = randint(datetime.now().hour, 22)
-    else:
-        hourstime = randint(9, 22)
+        daytime = datetime.now().day
+    else:"""
+    hourstime = randint(9, 22)
+    daytime = datetime.now().day + 1
     minutstime = randint(0, 59)
     secundstime = randint(0, 59)
     timesos = datetime(datetime.now().year, 
                         datetime.now().month,
-                        datetime.now().day,
+                        daytime,
                         hourstime, 
                         minutstime,
                         secundstime)
-    return timesos.strftime("%H:%M")
+    return timesos.strftime("%d-%H-%M")
 
 def get_com():
     with open('commentSos.txt', 'r') as f:
--- myBot/main.py
+++ myBot/main.py
@@ -13,11 +13,16 @@ API_TOKEN = '8079636146:AAGihQJ-Vfzse59rGP6TnOj3BiPo1q_eOFo'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-bd.db_create()
+bd.chats_create()
+bd.db_user_create()
+bd.db_user_chat_connect_create()
 
 dp.register_message_handler(handlers.help, commands=["help"])
-dp.register_message_handler(get_comm.get_com, content_types=['photo'])
-dp.register_message_handler(get_comm.get_com, commands=["sosal"])
+dp.register_message_handler(get_comm.get_com, content_types=['photo'], commands=["sosal"])
+dp.register_message_handler(get_comm.get_comSOS, commands=["sosal"])
+dp.register_message_handler(get_comm.check_sos, commands=["now"])
+
+user_list = []
 
 @dp.message_handler(commands=['start'])
 async def command_start(message : types.Message):
@@ -28,22 +33,55 @@ async def command_start(message : types.Message):
     time_sos = gettime.get_time()
     bd.write_chat(time_sos, chat_id)
     while True:
-        await asyncio.sleep(60)
+        await asyncio.sleep(5)
         now = datetime.now()
-        current_time = now.strftime("%H:%M")
-        time_sos1 = bd.get_time(chat_id)
-        if current_time == time_sos1: 
+        current_time = now.strftime("%d-%H-%M")
+        time_sos = bd.get_time(chat_id)
+        if current_time == time_sos: 
+            global user_list
+            user_list = bd.get_users(message.chat.id)
             time_sos = gettime.get_time()
             bd.write_time(time_sos, chat_id)
-            await bot.send_message(message.chat.id, 
+            await bot.send_message(chat_id, 
                                    '@all Время сосать, господа!!! ') 
+            
+    
             
 @dp.message_handler(commands=['check'])
 async def check(message: types.Message):
     chat_id = message.chat.id
-
     await message.answer(f" Время следующего сосания: " + bd.get_time(chat_id)[0])
 
+@dp.message_handler(commands=['now_check'])
+async def check1(message: types.Message):
+    chat_id = message.chat.id  # Получаем ID чата
+    new_time_sos = datetime.now().strftime("%d-%H-%M")  # Записываем текущее время
+    bd.write_time(new_time_sos, chat_id)  # Записываем время в базу данных
+    await message.answer("Время обновлено!")
+
+"""@dp.message_handler(commands=['now_check'])
+async def check1(message: types.Message):
+    global time_sos
+    time_sos = datetime.now().strftime("%d-%H-%M")"""
+
+@dp.message_handler(commands=['get_user'])
+async def check2(message: types.Message):
+    global user_list
+    await message.answer(user_list)
+
+@dp.message_handler(content_types=types.ContentType.PHOTO)  
+async def echo_message(message: types.Message):
+    global user_list
+    if message.from_user.id in user_list:
+        user_list.pop(user_list.index(message.from_user.id))
+        await message.reply(f"Спасибо дорогой!")
+        await message.answer(get_comm.get_com_choice())
+
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def echo_message(message: types.Message):
+    global user_list
+    if message.from_user.id in user_list:
+        await message.reply(f"Скинь фото, олух")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
